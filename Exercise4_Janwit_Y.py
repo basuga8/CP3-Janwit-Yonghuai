b59c13318a4dc7afa6228bdf0114c10bf363e271
basuga8 <43886309+basuga8@users.noreply.github.com>
2023-09-18 21:10:05 +0700

    Create Exercise4_Janwit_Y.py

---
+++ Exercise4_Janwit_Y.py
@@ -0,0 +1,11 @@
+e = 80
+b = 75
+cs = 84
+cp = 79
+
+print("-------------------- Your Score --------------------")
+print("Foundation English :                ", e, 'คะแนน')
+print("General Business :                  ", b, 'คะแนน')
+print("Introduction to Computer Systems :  ", cs, 'คะแนน')
+print("Computer Programming :              ", cp, 'คะแนน')
+print("----------------------------------------------------")
