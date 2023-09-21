4cce647ea03d438959e5b4e0cda39dea135c0451
basuga8 <basuga8@gmail.com>
2023-09-21 20:52:23 +0700

    Create Exercise5_1_Janwit_Y.py
     Exercise5_1_ใช้งานประโยชน์จากตัวแปร และ ตัวดำเนินการ รวมถึงข้อมูลสร้างโปรแกรมสำคัญคำนวณเลข

---
+++ Exercise5_1_Janwit_Y.py
@@ -0,0 +1,10 @@
+n1 = int(input("Number_1: "))
+n2 = int(input("Number_2: "))
+re1 = n1 + n2
+re2 = n1 - n2
+re3 = n1 * n2
+re4 = n1 / n2
+print(n1, "+", n2, "=", re1)
+print(n1, "-", n2, "=", re2)
+print(n1, "*", n2, "=", re3)
+print(n1, "/", n2, "=", int(re4))
