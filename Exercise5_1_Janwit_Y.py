9f68faa0059e925f00c62e0473e1050b07b4f1b6
basuga8 <basuga8@gmail.com>
2023-09-23 22:21:36 +0700

    Update Exercise5_1_Janwit_Y.py

--- Exercise5_1_Janwit_Y.py
+++ Exercise5_1_Janwit_Y.py
@@ -1,11 +1,9 @@
-x = int(input("Number_1: "))
-y = int(input("Number_2: "))
-
+n1 = int(input("Number_1: "))
+n2 = int(input("Number_2: "))
 re1 = n1 + n2
 re2 = n1 - n2
 re3 = n1 * n2
 re4 = n1 / n2
-
 print(n1, "+", n2, "=", re1)
 print(n1, "-", n2, "=", re2)
 print(n1, "*", n2, "=", re3)
