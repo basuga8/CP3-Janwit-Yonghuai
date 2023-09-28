d94f527b9aebfa673f84b4e938a43ac85958b399
basuga8 <basuga8@gmail.com>
2023-09-28 21:15:36 +0700

    Update Exercise11_janwit_y.py

--- Exercise11_janwit_y.py
+++ Exercise11_janwit_y.py
@@ -5,10 +5,3 @@ for row in range(1,numm+1):
     for x in range(1):
         x = 2 * row - 1
     print(column + ("*" * x))
-
-print("========================================")
-
-# ย่อได้อีก
-num = int(input())
-for y in range(num):
-    print(" "*(num-y),"*"*((y*2)+1))
