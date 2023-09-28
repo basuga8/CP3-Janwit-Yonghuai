571c2c828cf8cb6d552d13e0bccbeeca8f1403a4
basuga8 <basuga8@gmail.com>
2023-09-28 21:14:51 +0700

    Update Exercise11_janwit_y.py
    แก้ variable

--- Exercise11_janwit_y.py
+++ Exercise11_janwit_y.py
@@ -1,7 +1,14 @@
-input = 5
-for row in range(1,input+1):
+numm = 5
+for row in range(1,numm+1):
     for column in range(1):
-        column = " " * (input-row)
+        column = " " * (numm-row)
     for x in range(1):
-        x = (2 * row - 1)
+        x = 2 * row - 1
     print(column + ("*" * x))
+
+print("========================================")
+
+# ย่อได้อีก
+num = int(input())
+for y in range(num):
+    print(" "*(num-y),"*"*((y*2)+1))
