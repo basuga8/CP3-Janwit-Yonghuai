7a3c93ddddf4da7fe5a9c59df0b0535be237671f
basuga8 <basuga8@gmail.com>
2023-09-30 19:48:09 +0700

    Create L50_create_function.py

---
+++ L50_create_function.py
@@ -0,0 +1,32 @@
+def num_plus(
+        x = int(input("+Num1 = ")),
+        y = int(input("+num1 = "))
+):
+    print("result(+) =", x + y)
+print("===========================================")
+
+def num_minus(
+        x = int(input("-Num1 = ")),
+        y = int(input("-num1 = "))
+):
+    print("result(-) =", x - y)
+print("===========================================")
+
+def num_multiply(
+        x = int(input("xNum1 = ")),
+        y = int(input("xnum1 = "))
+):
+    print("result(x) =", x * y)
+print("===========================================")
+
+def num_divide(
+        x = int(input("÷Num1 = ")),
+        y = int(input("÷num1 = "))
+):
+    print("result(÷) =", x / y)
+print("===========================================")
+
+num_plus()
+num_minus()
+num_multiply()
+num_divide()
