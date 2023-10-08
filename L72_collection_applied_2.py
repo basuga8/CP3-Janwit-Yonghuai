dab867ac5b51c5ac3a183a574f27fea295f980a0
basuga8 <basuga8@gmail.com>
2023-10-08 15:43:27 +0700

    Create L72_collection_applied_2.py

---
+++ L72_collection_applied_2.py
@@ -0,0 +1,22 @@
+menuList = []
+def showBill():
+    total_p = 0
+    print("My Food".center(len("My Food")+10,"+"))
+    for number in menuList:
+        print(number[0], "=", number[1], "บาท")
+        total_p += int(number[1])
+    print("Total:", total_p, "บาท")
+
+while True:
+    menuN = input("Please Enter Menu: ")
+    if menuN.lower() == "e":
+        break
+    else:
+        menuP = input("Enter Price: ")
+        menuList.append([menuN, menuP])
+
+showBill()
+
+print("=========================")
+print(menuList)
+
